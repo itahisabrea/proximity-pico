2d45f92d1a7f36bcd643d499fedabf49dd1fa27b
Itah <itahisa.brea@gmail.com>
2025-12-10 10:39:27 +0100

    Create main.c

---
+++ src/app/main.c
@@ -0,0 +1,18 @@
+/* * Archivo: main.c
+ * Descripción: Cerebro principal del sistema
+ */
+
+#include <stdio.h> 
+
+// TODO: Aquí tengo que incluir los archivos del HAL (hal_sensor.h, etc)
+
+void main(void) {
+    // 1. Aquí irá la inicialización del hardware
+    
+    // 2. Aquí irá el bucle infinito (while true)
+    while (1) {
+        // A. Leer sensores
+        // B. Decidir estado (Verde/Ambar/Rojo)
+        // C. Actuar sobre salidas
+    }
+}

9613fd93ae5b1bbf966391b9976a33778cfd3ca9
Itah <itahisa.brea@gmail.com>
2025-12-10 11:09:30 +0100

    Update hal_output.h

--- src/hal/hal_output.h
+++ src/hal/hal_output.h
@@ -1,27 +1,29 @@
 /*
  * Archivo: hal_output.h
- * Descripción: Interfaz de abstracción para todas las salidas (LEDs, Buzzer, Pantalla).
+ * Descripción: Manejo de LEDs, Buzzer y Pantalla.
  */
 
 #ifndef HAL_OUTPUT_H
 #define HAL_OUTPUT_H
 
-// Define los 3 estados principales del sistema
+// --- DEFINICIÓN DE PINES  ---
+#define PIN_LED_RED     16  // GP16
+#define PIN_LED_AMBER   17  // GP17
+#define PIN_LED_GREEN   18  // GP18
+#define PIN_BUZZER      15  // GP15
+
+// Pantalla I2C (GP0 y GP1)
+#define I2C_PORT        i2c0
+#define PIN_I2C_SDA     0
+#define PIN_I2C_SCL     1
+
 typedef enum {
-    STATE_GREEN,    // Distancia segura
-    STATE_AMBER,    // Distancia de precaución
-    STATE_RED       // Distancia crítica
+    STATE_GREEN,
+    STATE_AMBER,
+    STATE_RED
 } SystemState_t;
 
-/**
- * @brief Inicializa el hardware de todas las salidas (GPIO de LEDs/Buzzer, I2C de pantalla).
- */
 void HAL_output_init(void);
-
-/**
- * @brief Actualiza todos los dispositivos de salida al nuevo estado.
- * @param state El estado a aplicar (VERDE, ÁMBAR o ROJO).
- */
 void HAL_set_system_state(SystemState_t state);
 
 #endif 

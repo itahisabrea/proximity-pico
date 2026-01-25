98f832944668caa8b6f16500c50190ebeeab5edd
RetornoDeLinea007 <238420503+RetornoDeLinea007@users.noreply.github.com>
2026-01-25 12:30:46 +0100

    Update hal_output.c
    Cambiados los mensajes y arreglada la funcion de estos

--- src/hal/hal_output.c
+++ src/hal/hal_output.c
@@ -7,6 +7,7 @@
 #include "hal/hal_output.h"
 #include "hal/pinout.h"
 #include "pico/stdlib.h"
+#include <string.h>
 #include "hardware/i2c.h"
 #include "drivers/oled/ssd1306.h"
 
@@ -18,8 +19,6 @@ struct render_area frame_area = {
 };
 
 uint8_t ssd[ssd1306_buffer_length]; // Buffer de renderizado de la pantalla
-// TODO: Cuando tengamos la librería de la pantalla, descomentad esta línea:
-// #include "drivers/oled/NOMBRELIBRERIA"
 
 void HAL_output_init(void) {
     // --- 1. CONFIGURACIÓN DE LEDS Y BUZZER ---
@@ -32,12 +31,6 @@ void HAL_output_init(void) {
     }
 
     // --- 2. CONFIGURACIÓN DE LA PANTALLA ---
-    /* * TAREA JOSECHU/PABLO: 
-     * Aquí tenemos que inicializar el I2C y la pantalla.
-     * Seguramente necesitemos algo parecido a esto:
-     */
-    
-    // i2c_init(I2C_PORT, 400 * 1000);
     i2c_init(i2c0, ssd1306_i2c_clock * 1000);
     gpio_set_function(0, GPIO_FUNC_I2C); // SDA
     gpio_set_function(1, GPIO_FUNC_I2C); // SCL
@@ -49,10 +42,15 @@ void HAL_output_init(void) {
     memset(ssd, 0, ssd1306_buffer_length); // Cero en el buffer del display
 }
 
-void HAL_msg(void) {
-    const char *message = "CHICKEN DETECTED";
+void oled_print_text(const char *msg) {
+    // 1. Limpiar el buffer (borrar lo anterior)
     memset(ssd, 0, ssd1306_buffer_length);
-    ssd1306_draw_string_absolute(ssd, 5, 1, message);
+    
+    // 2. Dibujar el nuevo texto
+
+    ssd1306_draw_string(ssd, 5, 0, (char*)msg); 
+    
+    // 3. Enviar a la pantalla
     render_on_display(ssd, &frame_area);
 }
 
@@ -67,21 +65,20 @@ switch (state) {
         case STATE_GREEN:
             // Caso Seguro
             gpio_put(LED_GREEN_PIN, 1);
-            // TODO: oled_print("SEGURO");
+            oled_print_text("SEGURO");
             break;
 
         case STATE_AMBER:
             // Caso Precaución
             gpio_put(LED_ORANGE_PIN, 1);
-            // Opcional: Pitido corto si queremos (por ahora está apagado)
-            // TODO: oled_print("CUIDADO");
+            oled_print_text("PRECAUCION");
             break;
 
         case STATE_RED:
             // Caso Peligro
             gpio_put(LED_RED_PIN, 1);
             gpio_put(BUZZER_PIN, 1); 
-            HAL_msg();
+            oled_print_text("PELIGRO");
             break;
     }
 }

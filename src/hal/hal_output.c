8d6a91bc25ec1dc47a26a66d5461b778003c4395
Itah <itahisa.brea@gmail.com>
2026-01-25 10:39:34 +0100

    feat(hal): unificación de control de LEDs y Buzzer
    Actualizado hal_output.c para gestionar estados de alarma (Buzzer en ROJO). Se preservan los bloques TODO para la integración de la pantalla OLED (tareas de Josechu/Pablo).

--- src/hal/hal_output.c
+++ src/hal/hal_output.c
@@ -5,6 +5,7 @@
  */
 
 #include "hal/hal_output.h"
+#include "hal/pinout.h"    // <--- IMPORTANTE: Aquí están definidos LED_RED_PIN, etc.
 #include "pico/stdlib.h"
 #include "hardware/i2c.h"
 
@@ -13,7 +14,8 @@
 
 void HAL_output_init(void) {
     // --- 1. CONFIGURACIÓN DE LEDS Y BUZZER ---
-    const uint pins_salida[] = {PIN_LED_RED, PIN_LED_AMBER, PIN_LED_GREEN, PIN_BUZZER};
+    // Usamos los nombres que definiste en pinout.h
+    const uint pins_salida[] = {LED_RED_PIN, LED_ORANGE_PIN, LED_GREEN_PIN, BUZZER_PIN};
     
     for (int i = 0; i < 4; i++) {
         gpio_init(pins_salida[i]);
@@ -27,42 +29,57 @@ void HAL_output_init(void) {
      * Seguramente necesitemos algo parecido a esto:
      */
     
-    // i2c_init(I2C_PORT, 400 * 1000);
-    // gpio_set_function(PIN_I2C_SDA, GPIO_FUNC_I2C);
-    // gpio_set_function(PIN_I2C_SCL, GPIO_FUNC_I2C);
-    // gpio_pull_up(PIN_I2C_SDA);
-    // gpio_pull_up(PIN_I2C_SCL);
+    // i2c_init(OLED_I2C_PORT, OLED_BAUDRATE); // Usando constantes de pinout.h
+    // gpio_set_function(OLED_SDA_PIN, GPIO_FUNC_I2C);
+    // gpio_set_function(OLED_SCL_PIN, GPIO_FUNC_I2C);
+    // gpio_pull_up(OLED_SDA_PIN);
+    // gpio_pull_up(OLED_SCL_PIN);
     
     // NOMBRELIBRERIA_init(); // <--- Llamada a la libreria
 }
 
 void HAL_set_system_state(SystemState_t state) {
-    // Apagamos todo para asegurar un estado limpio
-    gpio_put(PIN_LED_RED, 0);
-    gpio_put(PIN_LED_AMBER, 0);
-    gpio_put(PIN_LED_GREEN, 0);
-    gpio_put(PIN_BUZZER, 0);
+    // Apagamos todo para asegurar un estado limpio antes de encender lo nuevo
+    gpio_put(LED_RED_PIN, 0);
+    gpio_put(LED_ORANGE_PIN, 0);
+    gpio_put(LED_GREEN_PIN, 0);
+    gpio_put(BUZZER_PIN, 0);
 
-/* * TAREA JOSECHU/PABLO: 
+    /* * TAREA JOSECHU/PABLO: 
      * Rellenad el switch para encender lo que toque en cada caso.
-   
+     * NOTA DEL EQUIPO: Ya hemos dejado programados los LEDs y el Buzzer, 
+     * falta implementar la pantalla.
+     */
 
     switch (state) {
         case STATE_GREEN:
-            // TODO: Encender LED Verde
-            // TODO: Escribir "SEGURO o DISTANCIA SEGURA o lo que querais" en la pantallita
+            // TODO: Encender LED Verde -> HECHO
+            gpio_put(LED_GREEN_PIN, 1);
+            
+            // TODO: Escribir "SEGURO" en la pantallita
+            // oled_print("SEGURO");
             break;
 
         case STATE_AMBER:
-            // TODO: Encender LED Ámbar
-            // TODO: Hacer sonar el Buzzer bajito (opcional, esto hay que decidir que vamos a hacer)
-            // TODO: Escribir "PRECAUCION o CUIDADO" en la pantallita
+            // TODO: Encender LED Ámbar -> HECHO
+            gpio_put(LED_ORANGE_PIN, 1);
+            
+            // TODO: Hacer sonar el Buzzer bajito -> (De momento apagado en precaución)
+            // gpio_put(BUZZER_PIN, 0); 
+
+            // TODO: Escribir "PRECAUCION" en la pantallita
+            // oled_print("PRECAUCION");
             break;
 
         case STATE_RED:
-            // TODO: Encender LED Rojo
-            // TODO: Encender Buzzer a todo volumen ajajaj
-            // TODO: Escribir "STOP!! o PELIGRO" en la pantallita
+            // TODO: Encender LED Rojo -> HECHO
+            gpio_put(LED_RED_PIN, 1);
+            
+            // TODO: Encender Buzzer a todo volumen -> HECHO
+            gpio_put(BUZZER_PIN, 1); 
+            
+            // TODO: Escribir "PELIGRO" en la pantallita
+            // oled_print("PELIGRO");
             break;
     }
 }

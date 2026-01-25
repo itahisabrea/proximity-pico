bfccc00c22718183e8b282353438717e57dc403c
RetornoDeLinea007 <238420503+RetornoDeLinea007@users.noreply.github.com>
2026-01-25 12:00:01 +0100

    Update hal_output.c

--- src/hal/hal_output.c
+++ src/hal/hal_output.c
@@ -8,6 +8,14 @@
 #include "hal/pinout.h"
 #include "pico/stdlib.h"
 #include "hardware/i2c.h"
+#include "drivers/oled/ssd1306.h"
+
+struct render_area frame_area = {
+    start_column : 0,
+    end_column : ssd1306_width - 1,
+    start_page : 0,
+    end_page : ssd1306_n_pages - 1
+};
 
 // TODO: Cuando tengamos la librería de la pantalla, descomentad esta línea:
 // #include "drivers/oled/NOMBRELIBRERIA"
@@ -29,12 +37,15 @@ void HAL_output_init(void) {
      */
     
     // i2c_init(I2C_PORT, 400 * 1000);
-    // gpio_set_function(PIN_I2C_SDA, GPIO_FUNC_I2C);
-    // gpio_set_function(PIN_I2C_SCL, GPIO_FUNC_I2C);
-    // gpio_pull_up(PIN_I2C_SDA);
-    // gpio_pull_up(PIN_I2C_SCL);
-    
-    // NOMBRELIBRERIA_init(); // <--- Llamada a la libreria
+    i2c_init(i2c1, ssd1306_i2c_clock * 1000)
+    gpio_set_function(0, GPIO_FUNC_I2C); // SDA
+    gpio_set_function(1, GPIO_FUNC_I2C); // SCL
+    gpio_pull_up(0); // Pull-up para SDA
+    gpio_pull_up(1); // Pull-up para SCL
+
+    ssd1306_init();
+    calculate_render_area_buffer_length(&frame_area); // Preparar el area de renderiado par ael display
+    memset(ssd, 0, ssd1306_buffer_length); // Cero en el buffer del display
 }
 
 void HAL_set_system_state(SystemState_t state) {
